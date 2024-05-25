fa8fb955d3384dc9c96eca7dabd1ce57351286c5
Omer Akram <omer@thing.com>
2024-05-25 17:02:34 +0500

    add msgpack based message serializer

--- go.mod
+++ go.mod
@@ -2,6 +2,12 @@ module github.com/xconnio/wampproto-go
 
 go 1.20
 
-require github.com/fxamacker/cbor/v2 v2.6.0
+require (
+	github.com/fxamacker/cbor/v2 v2.6.0
+	github.com/vmihailenco/msgpack/v5 v5.4.1
+)
 
-require github.com/x448/float16 v0.8.4 // indirect
+require (
+	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
+	github.com/x448/float16 v0.8.4 // indirect
+)
---
+++ serializers/msgpack.go
@@ -0,0 +1,31 @@
+package serializers
+
+import (
+	"github.com/vmihailenco/msgpack/v5"
+
+	"github.com/xconnio/wampproto-go/messages"
+)
+
+type MsgPackSerializer struct{}
+
+func (m *MsgPackSerializer) Serialize(message messages.Message) ([]byte, error) {
+	return msgpack.Marshal(message.Marshal())
+}
+
+func (m *MsgPackSerializer) Deserialize(payload []byte) (messages.Message, error) {
+	var msgRaw []any
+	if err := msgpack.Unmarshal(payload, &msgRaw); err != nil {
+		return nil, err
+	}
+
+	msg, err := ToMessage(msgRaw)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = msg.Parse(msgRaw); err != nil {
+		return nil, err
+	}
+
+	return msg, nil
+}

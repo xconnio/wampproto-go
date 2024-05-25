a10bf83fbe75efc224bec3bd16839dddf704ea36
Omer Akram <omer@thing.com>
2024-05-26 03:00:13 +0500

    add client authenticator interface

---
+++ auth/authenticator.go
@@ -0,0 +1,10 @@
+package auth
+
+import "github.com/xconnio/wampproto-go/messages"
+
+type ClientAuthenticator interface {
+	AuthMethod() string
+	AuthID() string
+	AuthExtra() map[string]any
+	Authenticate(challenge messages.Challenge) (messages.Authenticate, error)
+}

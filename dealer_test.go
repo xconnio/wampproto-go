72fcc0ecbfb786f3c16bf7881fd533b85ebf4d64
Muzzammil Shahid <muzzammil@xconn.io>
2024-07-09 17:20:20 +0500

    Add more tests for dealer

--- dealer_test.go
+++ dealer_test.go
@@ -33,26 +33,93 @@ func TestDealerAddRemoveSession(t *testing.T) {
 func TestDealerRegisterUnregister(t *testing.T) {
 	dealer := wampproto.NewDealer()
 
+	callee := wampproto.NewSessionDetails(1, "realm", "authid", "anonymous", false)
+	err := dealer.AddSession(callee)
+	require.NoError(t, err)
+
+	var registerationID int64
+
 	t.Run("Register", func(t *testing.T) {
-		details := wampproto.NewSessionDetails(1, "realm", "authid", "anonymous", false)
-		err := dealer.AddSession(details)
-		require.NoError(t, err)
-
 		register := messages.NewRegister(1, nil, "foo.bar")
-		msg, err := dealer.ReceiveMessage(details.ID(), register)
+		msg, err := dealer.ReceiveMessage(callee.ID(), register)
 		require.NoError(t, err)
 		require.NotNil(t, msg)
-		require.Equal(t, msg.Recipient, details.ID())
-		require.Equal(t, msg.Message.Type(), messages.MessageTypeRegistered)
+		require.Equal(t, msg.Recipient, callee.ID())
+		require.Equal(t, messages.MessageTypeRegistered, msg.Message.Type())
 
-		register = messages.NewRegister(2, nil, "foo.bar")
-		msg, err = dealer.ReceiveMessage(details.ID(), register)
+		hasProcedure := dealer.HasProcedure("foo.bar")
+		require.True(t, hasProcedure)
+		registerationID = msg.Message.(*messages.Registered).RegistrationID()
+
+		t.Run("DuplicateProcedure", func(t *testing.T) {
+			register = messages.NewRegister(2, nil, "foo.bar")
+			msg, err = dealer.ReceiveMessage(callee.ID(), register)
+			require.NoError(t, err)
+			require.NotNil(t, msg)
+			require.Equal(t, msg.Recipient, callee.ID())
+			require.Equal(t, messages.MessageTypeError, msg.Message.Type())
+			errMsg := msg.Message.(*messages.Error)
+			require.NotNil(t, errMsg)
+			require.Equal(t, errMsg.URI(), "wamp.error.procedure_already_exists")
+		})
+
+		t.Run("InvalidSessionID", func(t *testing.T) {
+			invalidRegister := messages.NewRegister(2, nil, "foo.bar")
+			_, err = dealer.ReceiveMessage(5, invalidRegister)
+			require.EqualError(t, err, "cannot register procedure for non-existent session 5")
+		})
+	})
+
+	t.Run("Call", func(t *testing.T) {
+		caller := wampproto.NewSessionDetails(2, "realm", "authid", "anonymous", false)
+		err := dealer.AddSession(caller)
 		require.NoError(t, err)
-		require.NotNil(t, msg)
-		require.Equal(t, msg.Recipient, details.ID())
-		require.Equal(t, msg.Message.Type(), messages.MessageTypeError)
-		errMsg := msg.Message.(*messages.Error)
-		require.NotNil(t, errMsg)
-		require.Equal(t, errMsg.URI(), "wamp.error.procedure_already_exists")
+
+		call := messages.NewCall(3, map[string]any{}, "foo.bar", []any{"abc"}, nil)
+		invWithRecipient, err := dealer.ReceiveMessage(caller.ID(), call)
+		require.NoError(t, err)
+		require.NotNil(t, invWithRecipient)
+		require.Equal(t, callee.ID(), invWithRecipient.Recipient)
+		require.Equal(t, messages.MessageTypeInvocation, invWithRecipient.Message.Type())
+
+		// receive yield for invocation
+		invocation := invWithRecipient.Message.(*messages.Invocation)
+		yield := messages.NewYield(invocation.RequestID(), map[string]any{}, []any{"abc"}, nil)
+		yieldWithRecipient, err := dealer.ReceiveMessage(caller.ID(), yield)
+		require.NoError(t, err)
+		require.NotNil(t, yieldWithRecipient)
+		require.Equal(t, caller.ID(), yieldWithRecipient.Recipient)
+		require.Equal(t, messages.MessageTypeResult, yieldWithRecipient.Message.Type())
+
+		t.Run("NonExistingProcedure", func(t *testing.T) {
+			invalidCallMessage := messages.NewCall(3, map[string]any{}, "invalid", []any{"abc"}, nil)
+			errWithRecipient, err := dealer.ReceiveMessage(caller.ID(), invalidCallMessage)
+			require.NoError(t, err)
+			require.NotNil(t, errWithRecipient)
+			require.Equal(t, caller.ID(), errWithRecipient.Recipient)
+			require.Equal(t, errWithRecipient.Message.Type(), messages.MessageTypeError)
+		})
+
+		t.Run("InvalidYield", func(t *testing.T) {
+			_, err = dealer.ReceiveMessage(5, yield)
+			require.EqualError(t, err, "yield: not pending calls for session 5")
+		})
+	})
+
+	t.Run("Unregister", func(t *testing.T) {
+		unregister := messages.NewUnregister(callee.ID(), registerationID)
+		unregWithRecipient, err := dealer.ReceiveMessage(callee.ID(), unregister)
+		require.NoError(t, err)
+		require.NotNil(t, unregWithRecipient)
+		require.Equal(t, callee.ID(), unregWithRecipient.Recipient)
+		require.Equal(t, messages.MessageTypeUnregistered, unregWithRecipient.Message.Type())
+
+		hasProcedure := dealer.HasProcedure("foo.bar")
+		require.False(t, hasProcedure)
+
+		t.Run("InvalidRegistration", func(t *testing.T) {
+			_, err = dealer.ReceiveMessage(callee.ID(), unregister)
+			require.EqualError(t, err, "unregister: session 1 has no registration 1")
+		})
 	})
 }
